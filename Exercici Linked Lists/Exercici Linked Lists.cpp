91ae54c02892e04fe6dbfd5f5bf5f2586ae8837f
sergifonte <sergi.font.escoin@estudiantat.upc.edu>
2024-11-28 15:53:25 +0100

    fet

--- Exercici Linked Lists/Exercici Linked Lists.cpp
+++ Exercici Linked Lists/Exercici Linked Lists.cpp
@@ -52,8 +52,19 @@ void List::push_back(const int& value)
 {
     Node* new_node = new Node;
     new_node->value = value;
-   
+    new_node->next = nullptr;
+    new_node->prev = last;
 
+    if (last)
+    {
+        last->next = new_node;
+    }
+    else
+    {
+        first = new_node;
+    }
+    last = new_node;
+    num_elems++;
 }
 
 void List::insert(unsigned int position, const int& value)
@@ -66,8 +77,34 @@ void List::insert(unsigned int position, const int& value)
 
     Node* new_node = new Node;
     new_node->value = value;
-    
+    new_node->next = nullptr;
+    new_node->prev = nullptr;
 
+    if (position == 0)
+    {
+        new_node->next = first;
+        if (first)
+            first->prev = new_node;
+        first = new_node;
+        if (!last)
+            last = new_node;
+    }
+    else
+    {
+        Node* current = first;
+        for (unsigned int i = 0; i < position - 1; ++i)
+        {
+            current = current->next;
+        }
+        new_node->next = current->next;
+        new_node->prev = current;
+        if (current->next)
+            current->next->prev = new_node;
+        current->next = new_node;
+        if (new_node->next == nullptr)
+            last = new_node;
+    }
+    num_elems++;
 }
 
 void List::print() const
@@ -83,17 +120,33 @@ void List::print() const
 
 void List::clear()
 {
-    
+    while (first)
+    {
+        Node* temp = first;
+        first = first->next;
+        delete temp;
+    }
+    last = nullptr;
+    num_elems = 0;
 }
 
 int main()
 {
     List my_list;
 
-    my_list.insert(0, 10);
+    my_list.insert(0, 10); // Inserir al principi
     my_list.print();
 
-    my_list.insert(1, 20);
+    my_list.insert(1, 20); // Inserir al final
     my_list.print();
 
+    my_list.insert(1, 15); // Inserir al mig
+    my_list.print();
+
+    my_list.insert(0, 5); // Inserir al principi
+    my_list.print();
+
+    my_list.insert(2, 12); // Inserir al mig
+    my_list.print();
 }
+
